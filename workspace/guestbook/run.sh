c27e74f3a998432377489fd6b5830f35af8ae85e
juanfranrv <juanfranrv@correo.ugr.es>
2014-12-14 18:19:36 +0100

    añadiendo periodico

--- workspace/guestbook/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Variables
-PROJECT_DIR="apps/iv-aerospace/workspace/guestbook"
-APPENGINE_SERVER="apps/google_appengine/dev_appserver.py"
-
-# Lanzar aplicación (con autoconfirmación)
-
-echo y | python $APPENGINE_SERVER $PROJECT_DIR/src
