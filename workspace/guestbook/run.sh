ef681a13c611a27308a34dc59e6475c3b71177b6
Julio Martínez Martínez-Checa <julioxus@gmail.com>
2014-12-15 22:51:03 +0100

    Test que comprueba que la aplicación está funcionando

--- workspace/guestbook/run.sh
+++ workspace/guestbook/run.sh
@@ -6,4 +6,10 @@ APPENGINE_SERVER="apps/google_appengine/dev_appserver.py"
 
 # Lanzar aplicación (con autoconfirmación)
 
-echo y | python $APPENGINE_SERVER $PROJECT_DIR/src
+echo y | python $APPENGINE_SERVER $PROJECT_DIR/src &
+
+if [ $(curl localhost:8080 | wc -l) > 0 ]; then
+        echo  '\n LA WEB FUNCIONA!!! \n'
+else
+        echo "\n LA WEB NO FUNCIONA :S \n"
+fi
