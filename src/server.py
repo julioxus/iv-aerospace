4d22920aba046bde4688e88f5cef53cade8bd28d
juanfranrv <juanfranrv@correo.ugr.es>
2015-01-13 16:20:37 +0100

    Arreglando estructura del proyecto y añadiendo registro de usuarios con ndb ( a medias)

--- src/server.py
+++ src/server.py
@@ -16,7 +16,7 @@ class ErrorPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
         template_values={}
-        template = JINJA_ENVIRONMENT.get_template('error.html')
+        template = JINJA_ENVIRONMENT.get_template('template/error.html')
         self.response.write(template.render(template_values))
         
 class MainPage(webapp2.RequestHandler):
@@ -25,7 +25,16 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
         template_values={}
-        template = JINJA_ENVIRONMENT.get_template('index.html')
+        template = JINJA_ENVIRONMENT.get_template('template/index.html')
+        self.response.write(template.render(template_values))
+        
+class InfoPage(webapp2.RequestHandler):
+    
+    
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        template_values={}
+        template = JINJA_ENVIRONMENT.get_template('template/info_proyecto.html')
         self.response.write(template.render(template_values))
         
 class FormularioRegistro(webapp2.RequestHandler):
@@ -33,12 +42,57 @@ class FormularioRegistro(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
         template_values={}
-        template = JINJA_ENVIRONMENT.get_template('registro.html')
+        template = JINJA_ENVIRONMENT.get_template('template/registro.html')
+        self.response.write(template.render(template_values))
+
+class Usuario(ndb.Model):
+    usuario = ndb.StringProperty()
+    password = ndb.StringProperty()
+    nombre = ndb.StringProperty()
+    apellido = ndb.StringProperty()
+    correo = ndb.StringProperty()
+    telefono = ndb.StringProperty()
+
+class registro_usuario(webapp2.RequestHandler):
+    
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        template_values={}
+        template = JINJA_ENVIRONMENT.get_template('template/registro.html')
+        self.response.write(template.render(template_values))
+    def post(self):
+        user = Usuario()
+        user.usuario = self.request.get('usuario')
+        user.password = self.request.get('password')
+        user.nombre = self.request.get('nombre')
+        user.apellido = self.request.get('apellido')
+        user.correo = self.request.get('correo')
+        user.telefono = self.request.get('telefono')
+        print user.usuario
+        print user.password
+                        
+        user.put()
+
+        self.response.write('El usuario se ha registrado correstamente')
+
+class consulta_usuario(webapp2.RequestHandler):
+    def get(self):
+        usuarios = []
+        result = Usuario.query()
+        for usuario in result:
+            usuarios.append(usuario)
+            print usuario
+        self.response.headers['Content-Type'] = 'text/html'
+        template_values = {'usuarios':usuarios}
+        template = JINJA_ENVIRONMENT.get_template('template/consulta_usuario.html')
         self.response.write(template.render(template_values))
 
 urls = [('/',MainPage),
         ('/error',ErrorPage),
         ('/registrarse',FormularioRegistro),
+        ('/info_page',InfoPage),
+        ('/reg_usuario', registro_usuario),
+        ('/consulta_usuario', consulta_usuario),
        ]
 application = webapp2.WSGIApplication(urls, debug=True)
 
