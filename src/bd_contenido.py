6871b7a8a3fbd26f45c3157cacf3f4b56bcede98
miguel mc <miguelmc@correo.ugr.es>
2015-01-15 00:51:47 +0100

    añadiendo codigo para listar los datos
    #51 integrando código para poder realizar un listado de la información contenida en la base de datos

--- src/bd_contenido.py
+++ src/bd_contenido.py
@@ -84,13 +84,52 @@ class InsertarDatos(webapp2.RequestHandler):
          self.response.out.write("""<a href="/formulario">Insertar otro</a>""")
          self.response.out.write("""<a href="/">Volver al menú principal</a>""")
          self.response.out.write("</body></html>")
+
          
+class ListarDatos(webapp2.RequestHandler):
+     def get(self):
+         self.response.out.write("""
+         <html>
+             <head></head>
+         <body>
+             <table>
+                 <tr>
+                     <td><strong>Temperatura (ºC)</strong></td>
+                     <td><strong>Velocidad del viento (km/h)</strong></td>
+                     <td><strong>Dirección del viento</strong></td>
+                     <td><strong>Racha (km/h)</strong></td>
+                     <td><strong>Dirección de racha</strong></td>
+                     <td><strong>Precipitación (mm)</strong></td>
+                     <td><strong>Presión (hPa)</strong></td>
+                     <td><strong>Tendencia (hPa)</strong></td>
+                     <td><strong>Humedad (%)</strong></td>
+                 </tr>
+                 """)
+         lista = ndb.Query()
+         
+         
+         for datos in lista:
+             self.response.out.write("<tr>")
+             self.response.out.write("<td>" + str(datos.temperatura) + "</td>")
+             self.response.out.write("<td>" + str(datos.velocidadViento) + "</td>")
+             self.response.out.write("<td>" + datos.direccionViento + "</td>")
+             self.response.out.write("<td>" + str(datos.racha) + "</td>")
+             self.response.out.write("<td>" + datos.direccionRacha + "</td>")
+             self.response.out.write("<td>" + str(datos.precipitacion) + "</td>")
+             self.response.out.write("<td>" + str(datos.presion) + "</td>")
+             self.response.out.write("<td> " + str(datos.tendencia) + "</td>")
+             self.response.out.write("<td>" + str(datos.humedad) + "</td>")
+             self.response.out.write("</tr></table>")
+         
+         self.response.out.write("""<a href="/">Principal</a>""")
+         self.response.out.write("</body></html>")
+
 
 app = webapp2.WSGIApplication([
   ('/', Principal),
   ('/formulario', FormularioInsercion),
-  ('/guardarDatos', InsertarDatos),debug=True)
-  
+  ('/guardarDatos', InsertarDatos),
+  ('/listar', ListarDatos)], debug=True)
 
 def main():
     run_wsgi_app(app)
