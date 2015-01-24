c649c43617589403d3ba2e681b1f6f100c34f989
juanfranrv <juanfranrv@correo.ugr.es>
2015-01-24 11:46:21 +0100

    Probando ejecución de test con integración continua #54

--- test.py
+++ test.py
@@ -24,7 +24,7 @@ class  aerospaceTestCase(unittest.TestCase):
 	def test(self):
 		global usuario
 		pruebas = Tests()
-		response = pruebas.testBD(usuario)
+		response = pruebas.testBD(self,usuario)
 		self.assertEqual(response,True)
 
 if __name__ == '__main__':
