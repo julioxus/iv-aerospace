effeadde9b015703c9ec15c9e6d13e719763a4c2
julioxus <julioxus@gmail.com>
2015-01-28 09:31:07 +0100

    Intentando arreglar test cambiando nombre de app en la configuración #55

--- src/server.py
+++ src/server.py
@@ -661,7 +661,7 @@ class Tests(webapp2.RequestHandler):
         datastore_file = '/dev/null'
         from google.appengine.api import apiproxy_stub_map,datastore_file_stub
         apiproxy_stub_map.apiproxy = apiproxy_stub_map.APIProxyStubMap() 
-        stub = datastore_file_stub.DatastoreFileStub('myapp', datastore_file, '/')
+        stub = datastore_file_stub.DatastoreFileStub('ugraerospaceprogram', datastore_file, '/')
         apiproxy_stub_map.apiproxy.RegisterStub('datastore_v3', stub)
         
         # Introducimos el usuario inventado
