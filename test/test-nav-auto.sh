e4f2d836489ac75087eacfe0ddaacceddaaceb1e
osquiya <osquiya_6@hotmail.es>
2015-02-01 16:28:31 +0100

    arreglando test navegación y añadiendo script ejecutable

---
+++ test/test-nav-auto.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# instalamos el módulo de splinter para la ejecución del test
+
+sudo pip install splinter
+# ejecutamos el test que nos devuelve el tiempo medio de ejecución de una navegación automática de un usuario
+python test-nav-auto.py
